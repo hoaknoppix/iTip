f10524ca5cf7e44beb9e34e41d247cc59c43f929
Hoa Tran <hoaknoppix@gmail.com>
2016-05-14 10:59:05 +0700

    Update to the new swift version
    Initialize for polishing this app.

--- iTip/ViewController.swift
+++ iTip/ViewController.swift
@@ -16,8 +16,8 @@ class ViewController: UIViewController {
     var tipPercentage:Float = 5;
     @IBAction func editingChangeAction(sender: UITextField) {
         // I don't want to use currency format here since we must have another settings for selecting locale.
-        let orderCost:Float = (textBoxOrderCost.text as NSString).floatValue
-        tipPercentage  = (textBoxTipPercentage.text as NSString).floatValue
+        let orderCost:Float = (textBoxOrderCost.text! as NSString).floatValue
+        tipPercentage  = (textBoxTipPercentage.text! as NSString).floatValue
         labelTotal.text = String(format:"%.2f",(Util.getTotalCost(orderCost, tipPercentage: tipPercentage)))
     }
     override func viewDidLoad() {
@@ -36,9 +36,9 @@ class ViewController: UIViewController {
     }
     
     func loadTipPercentage(){
-        var defaults:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        let defaults:NSUserDefaults = NSUserDefaults.standardUserDefaults()
         tipPercentage = defaults.floatForKey("tip_percentage")
-                print("\(tipPercentage)")
+                print("\(tipPercentage)", terminator: "")
         if tipPercentage == 0 {
             tipPercentage = 5.0
         }
@@ -46,8 +46,8 @@ class ViewController: UIViewController {
     }
 
     func saveTipPercentage(){
-        var defaults:NSUserDefaults = NSUserDefaults.standardUserDefaults()
-        tipPercentage  = (textBoxTipPercentage.text as NSString).floatValue
+        let defaults:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        tipPercentage  = (textBoxTipPercentage.text! as NSString).floatValue
         defaults.setFloat(tipPercentage, forKey: "tip_percentage")
         defaults.synchronize()
     }
